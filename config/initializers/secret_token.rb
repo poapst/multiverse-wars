9253cdb259bd903d8262d80d17328f80c0468f2a
poapst <poapst@gmail.com>
2014-05-15 22:10:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Multiverse::Application.config.secret_key_base = '92a0e8bccf456486bed6d76369dd0a04fed41f57b21f45f57eb8e78212b3e0cfe0d642892095bd2f9aad041c6b75c8a373da2bc7a39899eb5f4ce9e62fdd0f59'
